400c31ead6549c037f44cb806d31f2a4becee8e1
Dakota <dakotamalchow@gmail.com>
2022-07-24 19:23:38 +0000

    Setup basic web app

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Doctor's appointment web app");
+});
+
+app.listen(3000,()=>{
+    console.log("App is listening on Port 3000");
+});
