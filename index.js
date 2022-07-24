491eb10b03c95a35fb5f6d5b0bf8b0cdd56a13f3
Dakota <dakotamalchow@gmail.com>
2022-07-24 19:47:25 +0000

    Setup doctor route

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const doctors = require("./doctors.json");
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Doctor's appointment web app");
+});
+
+app.get("/doctors/:doctorId", (req, res) => {
+    const doctor = doctors.find(d => d.doctorId == req.params.doctorId);
+    console.log("doctor:",doctor);
+    res.send(doctor);
 });
 
 app.listen(3000,()=>{
